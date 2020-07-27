dc6b869aecbc134d7b5470ae6c3423b8060c3cae
Raphael <paiva.raphaelt@gmail.com>
2020-07-28 01:32:14 +0200

    create server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
 import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-console.log(express);
+dotenv.config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(process.env.PORT, () => console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`));
