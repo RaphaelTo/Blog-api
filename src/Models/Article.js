f0411752a86f831179c4001cb6324bc13438883d
Raphael <paiva.raphaelt@gmail.com>
2020-07-28 01:46:37 +0200

    add key 'id, title, abstract, content, date'

--- src/Models/Article.js
+++ src/Models/Article.js
@@ -3,7 +3,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-
+    _id: String,
+    title: String,
+    abstract: String,
+    content: String,
+    date: { type: Date, default: Date.now},
 });
 
 export default ArticleSchema;
