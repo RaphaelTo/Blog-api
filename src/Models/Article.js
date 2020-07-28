31cde89451a0ac481c87a82e18ac7f5d4378bdd7
Raphael <paiva.raphaelt@gmail.com>
2020-07-28 02:02:54 +0200

    add max length

--- src/Models/Article.js
+++ src/Models/Article.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const ArticleSchema = new Schema({
     _id: String,
     title: String,
-    abstract: String,
+    abstract: { type: String, maxlength: 140},
     content: String,
     date: { type: Date, default: Date.now},
     Category : [{ type: Schema.Types.ObjectId, ref: 'Category'}]
