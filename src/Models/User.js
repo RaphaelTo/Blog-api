509919882974793e300c361591f2d87cb6ce7114
Raphael <paiva.raphaelt@gmail.com>
2020-07-28 02:03:20 +0200

    create User with key (username,pwd, id)

---
+++ src/Models/User.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    _id: String,
+    username: String,
+    password: { type: String, minlength: 8 }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
