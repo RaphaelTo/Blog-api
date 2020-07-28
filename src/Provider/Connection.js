dd2b83010ab194de8b20950eea09bb584c2b6536
Raphael <paiva.raphaelt@gmail.com>
2020-07-28 02:22:13 +0200

    create constructor

--- src/Provider/Connection.js
+++ src/Provider/Connection.js
@@ -1,5 +1,20 @@
 class Connection {
 
+    username;
+    password;
+    host;
+    port;
+    database;
+
+    constructor(logDB) {
+        const { username, password, host, port, database } = logDB;
+        this.username = username;
+        this.password = password;
+        this.host = host;
+        this.port = port;
+        this.database = database;
+    }
+
 }
 
 export default Connection;
