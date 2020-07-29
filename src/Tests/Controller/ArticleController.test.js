75ab2bb1608ed5ad5368466f014ffbab17ab1f4e
Raphael <paiva.raphaelt@gmail.com>
2020-07-30 01:59:20 +0200

    create ArticleController.test.js

---
+++ src/Tests/Controller/ArticleController.test.js
@@ -0,0 +1,12 @@
+import ArticleController from '../../Controllers/ArticleController';
+import mongoose from 'mongoose';
+
+jest.mock('mongoose');
+
+describe('test ArticleController class', () => {
+    test('implementation ArticleController', () => {
+        //Arrange
+        //Act
+        //Assert
+    })
+})
