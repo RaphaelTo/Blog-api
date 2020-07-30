a554bf6368e04345d99d1f2e4595a87750c4f358
Raphael <paiva.raphaelt@gmail.com>
2020-07-30 16:24:52 +0200

    create test throw error if this isnt instanceof model

--- src/Tests/Controller/ArticleController.test.js
+++ src/Tests/Controller/ArticleController.test.js
@@ -1,16 +1,21 @@
 import ArticleController from '../../Controllers/ArticleController';
 import mongoose from 'mongoose';
+import expect from 'expect';
 
 jest.mock('mongoose');
 
 describe('test ArticleController class', () => {
-    test('implementation ArticleController', () => {
+
+    test('throw error if param isnt mongoose instance', () => {
         //Arrange
-        const mockMongoose = mongoose.connect.mockReturnValue({});
-        //Act
-        const article = new ArticleController(mockMongoose);
-        //Assert
+        const mockMongoose = mongoose.model.mockReturnValue('Article');
+        
         expect(mockMongoose).toHaveBeenCalledTimes(1);
-        expect(article instanceof ArticleController).toBe(true);
+        
+        try {
+          new ArticleController(mockMongoose);
+        }catch (err) {
+            expect(err.message).toBe('ArticleControllerError: this isnt an mongoose model instance');
+        }
     })
 })
