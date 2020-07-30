f779cf897d21b37fde2fb98407457dc7f073f1e8
Raphael <paiva.raphaelt@gmail.com>
2020-07-30 02:04:35 +0200

    create first test 'test implementation ArticleController'

--- src/Tests/Controller/ArticleController.test.js
+++ src/Tests/Controller/ArticleController.test.js
@@ -6,7 +6,11 @@ jest.mock('mongoose');
 describe('test ArticleController class', () => {
     test('implementation ArticleController', () => {
         //Arrange
+        const mockMongoose = mongoose.connect.mockReturnValue({});
         //Act
+        const article = new ArticleController(mockMongoose);
         //Assert
+        expect(mockMongoose).toHaveBeenCalledTimes(1);
+        expect(article instanceof ArticleController).toBe(true);
     })
 })
