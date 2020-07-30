84de2f53c021237f949a6f92aaf4d9a16a48fa18
Raphael <paiva.raphaelt@gmail.com>
2020-07-30 18:16:30 +0200

    add route ArticleRoute.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+//Import module
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
@@ -6,11 +7,24 @@ import cors from 'cors';
 
 dotenv.config();
 
+//Import DB
+import Connection from "./Provider/Connection";
+import { identifiantDB } from "./Provider/identifiantDB";
+
+const mongo = new Connection(identifiantDB);
+
+mongo.connect();
+
+//Import Route
+import { anonymeRouteArticle } from "./Routes/ArticleRoute";
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(`${process.env.URL}/article`,anonymeRouteArticle);
 
 app.listen(process.env.PORT, () => console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`));
