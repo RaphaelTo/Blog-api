75855377e30c57c8bb58269a7a10a06e4f345cab
Raphael <paiva.raphaelt@gmail.com>
2020-07-30 16:25:37 +0200

    create construtor ArticleController.js

--- src/Controllers/ArticleController.js
+++ src/Controllers/ArticleController.js
@@ -1,5 +1,18 @@
+import Article from '../Models/Article';
+import ArticleControllerError from '../Errors/ArticleControllerError';
+import mongoose from 'mongoose';
+
 class ArticleController {
 
+    mongoose;
+
+    constructor(mongooseConnection) {
+        if(!mongooseConnection.prototype instanceof mongoose.Model) {
+            throw new ArticleControllerError('ArticleControllerError: this isnt an mongoose model instance');
+        }
+
+        this.mongoose = mongooseConnection;
+    }
 }
 
 export default ArticleController;
