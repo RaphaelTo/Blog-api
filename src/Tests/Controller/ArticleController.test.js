7c14a67e6953401af7e5755d55d88a54177950a6
Raphael <paiva.raphaelt@gmail.com>
2020-08-03 01:23:30 +0200

    create test "method getAllArticle return same value"

--- src/Tests/Controller/ArticleController.test.js
+++ src/Tests/Controller/ArticleController.test.js
@@ -6,6 +6,10 @@ jest.mock('mongoose');
 
 describe('test ArticleController class', () => {
 
+    beforeEach(() => {
+        jest.resetAllMocks();
+    })
+
     test('throw error if param isnt mongoose instance', () => {
         //Arrange
         const mockMongoose = mongoose.model.mockReturnValue('Article');
@@ -17,5 +21,100 @@ describe('test ArticleController class', () => {
         }catch (err) {
             expect(err.message).toBe('ArticleControllerError: this isnt an mongoose model instance');
         }
+    });
+
+    test('method getAllArticle return an object', async () => {
+        //Arrange
+        const mockModelMongoose = mongoose.model.mockReturnValue({});
+        const article = new ArticleController(mockModelMongoose);
+        //Act
+        const getAllArticle = article.getAllArticle();
+        //Assert
+        await expect(typeof getAllArticle).toBe('object');
+        await expect(getAllArticle).not.toBe(null);
+    });
+
+    test('method getAllArticle return same structure', async () => {
+        //Arrange
+        const structure = {
+            type : "success",
+            result: "api"
+        };
+        const mockModelMongoose = {
+            find: jest.fn().mockResolvedValue([
+                {
+                    "_id":"a",
+                    "Category":[],
+                    "title":"A",
+                    "abstract":"a",
+                    "content":"aaa",
+                    "date":{"$date":"2020-07-30T16:02:00.791Z"}},
+            ]),
+        };
+        const article = new ArticleController(mockModelMongoose);
+        //Act
+        expect.assertions(1);
+        const getAllArticle = Object.keys(await article.getAllArticle());
+        //Assert
+        expect(getAllArticle).toEqual(Object.keys(structure));
+    });
+
+    test('method getAllArticle return same value that mock', async () => {
+        //Arrange
+        const mockReturnAllArticle = [
+            {
+                "_id":"a",
+                "Category":[],
+                "title":"A",
+                "abstract":"a",
+                "content":"aaa",
+                "date":{"$date":"2020-07-30T16:02:00.791Z"}
+            },
+            {
+                "_id":"a",
+                "Category":[],
+                "title":"A",
+                "abstract":"a",
+                "content":"aaa",
+                "date":{"$date":"2020-07-30T16:02:00.791Z"}
+            },
+        ];
+
+        const mockModelMongooseWithQuery = {
+             find: jest.fn().mockResolvedValue([
+                 {
+                     "_id":"a",
+                     "Category":[],
+                     "title":"A",
+                     "abstract":"a",
+                     "content":"aaa",
+                     "date":{"$date":"2020-07-30T16:02:00.791Z"}
+                 },
+                 {
+                     "_id":"a",
+                     "Category":[],
+                     "title":"A",
+                     "abstract":"a",
+                     "content":"aaa",
+                     "date":{"$date":"2020-07-30T16:02:00.791Z"}
+                 },
+             ])
+         };
+
+        const article = new ArticleController(mockModelMongooseWithQuery);
+        const structure = (resultMock) => {
+             return {
+                 type : "success",
+                 result: resultMock
+             }
+         };
+
+        //Act
+        expect.assertions(2);
+        const getAllArticle = article.getAllArticle();
+
+        //Assert
+        await expect(getAllArticle).resolves.toEqual(structure(mockReturnAllArticle));
+        expect(mockModelMongooseWithQuery.find).toHaveBeenCalled();
     })
 })
