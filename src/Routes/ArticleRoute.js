2ea6eb46d3af0997889ddc096e917f9c2d8a6b67
Raphael <paiva.raphaelt@gmail.com>
2020-08-03 01:53:49 +0200

    create route for all articles

--- src/Routes/ArticleRoute.js
+++ src/Routes/ArticleRoute.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import ArticleController from "../Controllers/ArticleController";
+import Article from "../Models/Article";
 
 export const anonymeRouteArticle = express.Router();
 
 anonymeRouteArticle
-    .get('/all', (req, res) => {
-        console.log('hello all');
+    .get('/all', async (req, res) => {
+        const art = new ArticleController(Article);
+        res.status(200).json(await art.getAllArticle());
     });
