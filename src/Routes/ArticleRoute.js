cc7645b8d780f9660a71b0758fb721c94f4c8e68
Raphael <paiva.raphaelt@gmail.com>
2020-08-03 03:02:26 +0200

    add route userById

--- src/Routes/ArticleRoute.js
+++ src/Routes/ArticleRoute.js
@@ -8,4 +8,15 @@ anonymeRouteArticle
     .get('/all', async (req, res) => {
         const art = new ArticleController(Article);
         res.status(200).json(await art.getAllArticle());
-    });
+    })
+
+    .get('/byId/:id', async (req, res) => {
+        const article = new ArticleController(Article);
+        const byID = await article.getArticleById(req.params.id);
+
+        if(byID.type === "error"){
+            res.status(404).json(byID);
+        }else{
+            res.status(200).json(byID);
+        }
+    })
