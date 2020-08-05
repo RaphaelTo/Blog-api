0d684588b751158333d4a734837ddf57b8d836af
Raphael <paiva.raphaelt@gmail.com>
2020-08-06 01:21:14 +0200

    create route add delete put for ArticleRoute.js

--- src/Routes/ArticleRoute.js
+++ src/Routes/ArticleRoute.js
@@ -20,3 +20,37 @@ anonymeRouteArticle
             res.status(200).json(byID);
         }
     })
+
+    .post('/add', async (req, res) => {
+        const articleInstance = new Article(req.body);
+        const article = new ArticleController(articleInstance);
+        const createArticle = await article.createArticle();
+
+        if(createArticle.type === "error"){
+            res.status(400).json(createArticle);
+        }else {
+            res.status(201).json(createArticle);
+        }
+    })
+
+    .delete('/deleteById/:id', async (req, res) => {
+        const article = new ArticleController(Article);
+        const deleteArticle = await article.deleteArticleById(req.params.id);
+
+        if(deleteArticle.type === "error"){
+            res.status(404).json(deleteArticle);
+        }else {
+            res.status(200).json(deleteArticle);
+        }
+    })
+
+    .put('/updateById/:id', async (req, res) => {
+        const article = new ArticleController(Article);
+        const updateArticle = await article.updateArticleById(req.params.id, req.body);
+
+        if(updateArticle.type === "error"){
+            res.status(404).json(updateArticle);
+        }else {
+            res.status(200).json(updateArticle);
+        }
+    })
