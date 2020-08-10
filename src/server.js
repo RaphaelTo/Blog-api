641b298b0ea44f6137fbc45d35bf7c9616a91efc
Raphael <paiva.raphaelt@gmail.com>
2020-08-11 00:49:53 +0200

    add middlware JWT to check if token is here

--- src/server.js
+++ src/server.js
@@ -15,10 +15,13 @@ const mongo = new Connection(identifiantDB);
 
 mongo.connect();
 
+//Import middleware
+import { checkToken } from "./Middleware/JWT";
+
 //Import Route
-import { anonymeRouteArticle } from "./Routes/ArticleRoute";
-import { anonymeRouteCategory } from "./Routes/CategoryRoute";
-import { anonymeRouteUser } from "./Routes/UserRoute";
+import { anonymeRouteArticle, adminRouteArticle } from "./Routes/ArticleRoute";
+import { anonymeRouteCategory, adminRouteCategory } from "./Routes/CategoryRoute";
+import { anonymeRouteUser, adminRouteUser } from "./Routes/UserRoute";
 
 const app = express();
 
@@ -30,5 +33,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(`${process.env.URL}/article`, anonymeRouteArticle);
 app.use(`${process.env.URL}/category`, anonymeRouteCategory);
 app.use(`${process.env.URL}/user`, anonymeRouteUser);
+app.use(`${process.env.URL}/admin/article`, checkToken, adminRouteArticle);
+app.use(`${process.env.URL}/admin/category`, checkToken, adminRouteCategory);
+app.use(`${process.env.URL}/admin/user`, checkToken, adminRouteUser);
+
 
 app.listen(process.env.PORT, () => console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`));
